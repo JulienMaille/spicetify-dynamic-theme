01003756936353a9a929b903718ee404c93b55f0
Julien Maille <julien.maille@gmail.com>
2024-10-12 10:22:42 +0200

    IMP: patch dark mode cleanup

--- patch-dark-mode.ps1
+++ patch-dark-mode.ps1
@@ -1,19 +1,21 @@
-Get-Process -Name Spotify -ErrorAction SilentlyContinue | Stop-Process -Force
-Get-Process -Name SpotifyWebHelper -ErrorAction SilentlyContinue | Stop-Process -Force
+# Terminate Spotify processes
+Get-Process -Name Spotify, SpotifyWebHelper -ErrorAction SilentlyContinue | Stop-Process -Force
 
+# Backup Spotify executable
 $sp = spicetify config spotify_path
 $sp += "\Spotify.exe"
 Copy-Item $sp ($sp + ".backup")
 
-$bytes = [System.IO.File]::ReadAllBytes($sp);
-$toRemove = [System.Text.Encoding]::UTF8.GetBytes("force-dark-mode");
+# Byte operations
+$bytes = [System.IO.File]::ReadAllBytes($sp)
+$toRemove = [System.Text.Encoding]::UTF8.GetBytes("force-dark-mode")
 
+# Progress setup
 $sw = [System.Diagnostics.Stopwatch]::StartNew()
 for ($i = 0; $i -lt $bytes.Length; $i++) {
-    if ($sw.Elapsed.TotalMilliseconds -ge 1000) {
+    if ($i -eq 0 -or $sw.Elapsed.TotalMilliseconds -ge 2000) {
         Write-Progress -Activity "Enabling dark mode in Spotify.exe" -status "Patching binary file $i" -percentComplete ($i / $bytes.Length*100);
-        $sw.Reset();
-        $sw.Start();
+        $sw.Restart();
     }
     $found = $true
     for ($j = 0; $j -lt $toRemove.Length; $j++) {
@@ -31,9 +33,12 @@ for ($i = 0; $i -lt $bytes.Length; $i++) {
     }
 }
 
-[System.IO.File]::WriteAllBytes($sp, $bytes);
-if ( $found ) {
+# Write patched bytes back
+[System.IO.File]::WriteAllBytes($sp, $bytes)
+
+# Display result
+if ($found) {
     Write-Host "The patch is complete." -ForegroundColor "Green"
 } else {
     Write-Host "Failed to patch the file." -ForegroundColor "Red"
-} 
+}
