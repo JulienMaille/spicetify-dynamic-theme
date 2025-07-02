5e591fd5fdbd1cd0f3cf7aed0fe93047fc8cf91f
Julien Maille <julien.maille@gmail.com>
2025-07-02 18:28:46 +0200

    FIX: #120 Hide banners and gradients

--- default-dynamic.js
+++ default-dynamic.js
@@ -49,7 +49,7 @@ function lightenDarkenColor(h, p) {
 }
 
 function rgbToHsl([r, g, b]) {
-    (r /= 255), (g /= 255), (b /= 255);
+    ((r /= 255), (g /= 255), (b /= 255));
     var max = Math.max(r, g, b),
         min = Math.min(r, g, b);
     var h,
